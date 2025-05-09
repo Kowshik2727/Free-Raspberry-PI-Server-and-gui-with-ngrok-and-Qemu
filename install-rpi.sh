c626eb68dc8d63fe84d2611819dd5cf77e3483cc
Kowshik2727 <kifayatqadeer744@gmail.com>
2025-05-09 10:07:49 +0600

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -1,14 +1,13 @@
-NGROK_AUTH_TOKEN=your authtoken
 echo "Installing!!!" \
 sudo apt update \
-sudo apt install qemu-system-arm \
+sudo apt install qemu-system-arm -y \
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
   && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
-  && sudo apt install ngrok \
-ngrok config add-authtoken $NGROK_AUTH_TOKEN \
+  && sudo apt install ngrok -y \
+ngrok config add-authtoken \
 wget "https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/refs/heads/master/kernel-qemu-5.4.51-buster" \
 wget "https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/refs/heads/master/versatile-pb-buster.dtb" \
 wget "https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-01-12/2021-01-11-raspios-buster-armhf.zip" \
